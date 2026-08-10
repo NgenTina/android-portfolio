b001674ce138fcb1d297419aa6ac1c60ec8baf36
Ngen Tina <ngentina12345@gmail.com>
2026-08-10 22:26:45 +0700

    change structure

--- app/src/main/java/com/example/android_portfolio/core/models/ProjectItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android_portfolio.data.models
-
-interface ProjectItem {
-    val id: String
-    val title: String
-    val description: String
-    val category: String
-    val date: String
-}
