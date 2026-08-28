3b2219479263fab03df7064e7efaa96eda060f1d
Ngen Tina <ngentina12345@gmail.com>
2026-08-28 21:22:40 +0700

    add Bottom Navigation bar

---
+++ app/src/main/java/com/example/androidPortfolio/features/ingredients/navigationBar/ScreenBottomNavigationBar.kt
@@ -0,0 +1,120 @@
+package com.example.androidPortfolio.features.ingredients.navigationBar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidPortfolio.core.ui.theme.PortfolioTheme
+
+
+data class BottomNavItem(
+    val label: String,
+    val icon: ImageVector
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenBottomNavigationBar(
+    onBack: () -> Unit,
+    title: String = "Menu",
+) {
+
+    val bottomNavItems = listOf(
+        BottomNavItem("Home", Icons.Default.Home),
+        BottomNavItem("Profile", Icons.Default.Person),
+        BottomNavItem("Settings", Icons.Default.Settings)
+    )
+
+    var selectedItemIndex by remember { mutableStateOf(0) }
+
+    Scaffold(
+        // Top App Bar
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                ),
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                bottomNavItems.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = { selectedItemIndex = index },
+                        icon = {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.label,
+                            )
+                        },
+                        label = { Text(item.label) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center,
+        ) {
+            when (selectedItemIndex) {
+                0 -> Text("Home Screen")
+                1 -> Text("Profile Screen")
+                2 -> Text("Settings Screen")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScreenBottomNavigationBarPreview() {
+    PortfolioTheme {
+        ScreenBottomNavigationBar(
+            onBack = {},
+            title = "Menu Screen",
+        )
+    }
+}
